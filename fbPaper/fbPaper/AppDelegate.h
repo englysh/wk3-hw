01bcf1ab2fc878a9cce1e0930d58d0b8d9cc8402
englysh <englysengly@gmail.com>
2014-06-22 13:34:04 -0700

    first commit
    home work wk 3:
    -Dragging the headline should reveal the menu
    If you're dragging down, releasing the headline should animate the
    headline to the down position
    -If you're dragging up, releasing the headline should animate the
    headline to the up position
    -If you're dragging the headline up past the top, the friction should
    increase

---
+++ fbPaper/fbPaper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  fbPaper
+//
+//  Created by Engly Chang on 6/20/14.
+//  Copyright (c) 2014 Engly Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ fbPaper/fbPaper/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  fbPaper
+//
+//  Created by Engly Chang on 6/21/14.
+//  Copyright (c) 2014 Engly Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
